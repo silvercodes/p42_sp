16c410b14a1f4bd5db7634b961aa214a92dc7b27
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-06-09 23:06:53 +0300

    add:thread

---
+++ 02_thread/01_thread/Program.cs
@@ -0,0 +1,53 @@
+﻿
+#region intro
+
+//void ShowPlus()
+//{
+//    for (int i = 0; i < 1000; ++i)
+//        Console.Write('+');
+//}
+
+//Thread t = new Thread(ShowPlus);
+//t.Start();
+
+//Console.WriteLine(t.IsAlive);
+
+//for (int i = 0; i < 1000; ++i)
+//    Console.Write('0');
+
+
+
+
+//void Run()
+//{
+//    for (int i = 0; i < 5; ++i)
+//        Console.Write('0');
+//}
+//new Thread(Run).Start();
+//Run();
+//Console.WriteLine("Main end");
+
+
+
+
+
+bool done = false;
+
+void Run()
+{
+    if (!done)
+    {
+        // Console.WriteLine("+");
+        done = true;
+        Console.WriteLine("DONE");
+    }
+}
+
+new Thread(Run).Start();
+Run();
+
+
+#endregion
+
+
+
